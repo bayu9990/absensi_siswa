92245d58696e578a38881075d5aa1fe9c2141b3d
Bayu S <khususpby@gmail.com>
2024-05-16 12:53:02 +0700

    initial commit

---
+++ src/main/java/com/mycompany/absen_siswa/Absen_siswa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.absen_siswa;
+
+/**
+ *
+ * @author khusu
+ */
+public class Absen_siswa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
